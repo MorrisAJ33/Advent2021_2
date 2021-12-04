ef32f7df08f77cf481c693489a9bba52e9522c78
morri <morrisaj18@gmail.com>
2021-12-04 13:08:16 -0700

    Day3, part 2

--- D3P2.py
+++ D3P2.py
@@ -5,6 +5,7 @@ C02 = ''
 index = 0
 for p in range(0, 12):
     print(f'p= {p}')
+    print(lines)
     zeros = 0
     ones = 0
     if len(lines) > 2:
@@ -13,7 +14,7 @@ for p in range(0, 12):
                 zeros += 1
             else:
                 ones += 1
-        if zeros > ones:
+        if zeros < ones:
             oxygen = oxygen + '0'
         else:
             oxygen = oxygen + '1'
@@ -22,7 +23,7 @@ for p in range(0, 12):
         print(f'dropper value = {dropper}')
         lines_len = len(lines)
         print(f'{p} stage length = {lines_len}')
-        for d in range(0, lines_len - 1):
+        for d in range(0, lines_len):
             # print(f'd= {d}')
             # print(f'line = {lines[d]}')
             if lines[d][p] != oxygen[p]:
@@ -33,10 +34,10 @@ for p in range(0, 12):
         for i in drops:
             lines.pop(i)
         print(f'O2 current = {oxygen}')
-    elif len(lines) == 2:
+    elif len(lines) <= 2:
         if lines[0][p] == '1':
             oxygen = lines[0]
-        else:
+        elif len(lines) == 2 and lines[1][p] == '1':
             oxygen = lines[1]
 print(f'oxygen_string= {oxygen}')
 oxygen = int(oxygen[:11], 2)
@@ -45,6 +46,7 @@ file3_2 = open('D3_input.txt', 'r')
 lines2 = file3_2.readlines()
 for p in range(0, 12):
     print(f'p={p}')
+    print(lines2)
     zeros = 0
     ones = 0
     if len(lines2) > 2:
@@ -53,7 +55,7 @@ for p in range(0, 12):
                 zeros += 1
             else:
                 ones += 1
-        if zeros < ones:
+        if zeros > ones:
             C02 += '0'
         else:
             C02 += '1'
@@ -63,7 +65,7 @@ for p in range(0, 12):
         print(f'dropper value = {dropper}')
         lines2_len = len(lines2)
         print(f'{p} stage length = {lines2_len}')
-        for d in range(0, lines2_len - 1):
+        for d in range(0, lines2_len):
             if lines2[d][p] != C02[p]:
                 dropping.insert(index2, d)
                 index2 += 1
@@ -72,10 +74,10 @@ for p in range(0, 12):
         for i in dropping:
             lines2.pop(i)
         print(f'CO2 current = {C02}')
-    elif len(lines2) == 2:
+    elif len(lines2) <= 2:
         if lines2[0][p] == '0':
             C02 = lines2[0]
-        else:
+        elif len(lines2) == 2 and lines2[1][p] =='0':
             C02 = lines2[1]
 print(f'CO2 Final = {C02}')
 C02 = int(C02[:11], 2)
